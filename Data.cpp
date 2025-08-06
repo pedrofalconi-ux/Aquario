73445755b709a9285c9f69c8f377190a8b5746cc
Segunddo <antoniosegundo.887@gmail.com>
2025-08-05 23:12:26 -0300

    Acho que agora pega em linux tbm...

--- Data.cpp
+++ Data.cpp
@@ -1,29 +1,46 @@
-#include <iostream>
-#include <windows.h>
-#include <sstream>
-#include <iomanip>
 #include "Data.hpp"
 
-using namespace std;
-
 int dia, mes, ano, hora, minuto;
-SYSTEMTIME systemInfo;
+int day, month, year, hour, minute;
+
+void Data::atualizarData() {
+    #ifdef _WIN64
+        // --- CÓDIGO PARA WINDOWS ---
+        SYSTEMTIME st;
+        GetLocalTime(&st);
+        day = st.wDay;
+        month = st.wMonth;
+        year = st.wYear;
+        hour = st.wHour;
+        minute = st.wMinute;
+    #else
+        // --- CÓDIGO PARA LINUX ---
+        time_t tempo_bruto = time(NULL);
+        tm* tm_info = localtime(&tempo_bruto);
+        day = tm_info->tm_mday;
+        month = tm_info->tm_mon + 1;      // tm_mon é 0-11, então somamos 1
+        year = tm_info->tm_year + 1900;  // tm_year são os anos desde 1900
+        hour = tm_info->tm_hour;
+        minute = tm_info->tm_min;
+    #endif
+}
 
 void Data::setData(int d, int m, int a) {
-    GetLocalTime(&systemInfo);
+
+    atualizarData();
 
     //Ano
     if (a < 0) {
-        ano = systemInfo.wYear;
+        ano = year;
     }
-    else if (a > systemInfo.wYear) {
-        ano = systemInfo.wYear;
+    else if (a > year) {
+        ano = year;
     }
-    else if (a >= 0 && a <= systemInfo.wYear % 100) {
-        ano = systemInfo.wYear - (systemInfo.wYear % 100) + a; // Ex: se botar 24 ele joga a data para 2024
+    else if (a >= 0 && a <= year % 100) {
+        ano = year - (year % 100) + a; // Ex: se botar 24 ele joga a data para 2024
     }
-    else if (a > systemInfo.wYear % 100 && a < 100) {
-        ano = systemInfo.wYear - (systemInfo.wYear % 100) + (a-100); // Ex: se botar 95 ele joga a data para 1995
+    else if (a > year % 100 && a < 100) {
+        ano = year - (year % 100) + (a-100); // Ex: se botar 95 ele joga a data para 1995
     }
     else {
         ano = a;
@@ -31,7 +48,7 @@ void Data::setData(int d, int m, int a) {
 
     //Mês
     if (m < 1 || m > 12) {
-        mes = systemInfo.wMonth;
+        mes = month;
     }
     else {
         mes = m;
@@ -39,7 +56,7 @@ void Data::setData(int d, int m, int a) {
 
     //Dia
     if (d < 1 || d > 31) {
-        dia = systemInfo.wDay;
+        dia = day;
     } 
     else if (mes >= 1 && mes <= 7) {
         if (mes == 2) {
@@ -47,7 +64,7 @@ void Data::setData(int d, int m, int a) {
                 dia = d;
             }
             else if (d > 28) {
-                dia = systemInfo.wDay;
+                dia = day;
             }
             else {
                 dia = d;
@@ -55,7 +72,7 @@ void Data::setData(int d, int m, int a) {
         }
         else if (mes % 2 == 0) {
             if (d > 30) {
-                dia = systemInfo.wDay;
+                dia = day;
             }
             else {
                 dia = d;
@@ -68,7 +85,7 @@ void Data::setData(int d, int m, int a) {
     else {
         if (mes % 2 != 0) {
             if (d > 30) {
-                dia = systemInfo.wDay;
+                dia = day;
             }
             else {
                 dia = d;
@@ -81,9 +98,10 @@ void Data::setData(int d, int m, int a) {
 }
 
 void Data::setHora(int h, int mi) {
+    atualizarData();
     //Hora
     if (h < 0 || h > 24) {
-        hora = systemInfo.wHour;
+        hora = hour;
     }
     else {
         hora = h;
@@ -91,7 +109,7 @@ void Data::setHora(int h, int mi) {
 
     //Min
     if (mi < 0 || mi > 59) {
-        minuto = systemInfo.wMinute;
+        minuto = minute;
     }
     else {
         minuto = mi;
@@ -113,6 +131,27 @@ string Data::getHora() {
     // Formatação
     tempo << setw(2) << setfill('0') << hora << ":"
           << setw(2) << setfill('0') << minuto;
+
+    return tempo.str();
+}
+
+string Data::getDataDeHoje() {
+    atualizarData();
+    stringstream data;
+    // Formatação
+    data << setw(2) << setfill('0') << day << "/"
+         << setw(2) << setfill('0') << month << "/"
+         << setw(4) << setfill('0') << year;
+    
+    return data.str();
+}
+
+string Data::getHoraDeHoje() {
+    atualizarData();
+    stringstream tempo;
+    // Formatação
+    tempo << setw(2) << setfill('0') << hour << ":"
+          << setw(2) << setfill('0') << minute;
 
     return tempo.str();
 }
--- Data.hpp
+++ Data.hpp
@@ -2,34 +2,37 @@
 #define DATA_HPP
 
 #include <iostream>
-#include <windows.h>
 #include <sstream>
 #include <iomanip>
 #include "Data.hpp"
+#ifdef _WIN64
+    #include <windows.h> // Para Windows
+#else
+    #include <ctime>     // Para Linux e outros sistemas POSIX
+#endif
 
 using namespace std;
 
 class Data {
 private:
     int dia, mes, ano, hora, minuto;
-    SYSTEMTIME systemInfo;
+    int day, month, year, hour, minute;
+    void atualizarData();
 
 public:
 
     Data() {
-    GetLocalTime(&systemInfo);
-    dia = systemInfo.wDay;
-    mes = systemInfo.wMonth;
-    ano = systemInfo.wYear;
-    hora = systemInfo.wHour;
-    minuto = systemInfo.wMinute;
+        void atualizarData();
     }
 
     string getData();
     string getHora();
+    string getDataDeHoje();
+    string getHoraDeHoje();
 
     void setData(int d, int m, int a);
     void setHora(int h, int m);
+
 };
 
 #endif
